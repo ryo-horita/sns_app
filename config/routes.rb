ce576e2d8362ab8d857a332c102a4132ea6c701f
ryohorita <ryopiko2021@gmail.com>
2023-08-26 14:21:58 +0000

    ルーティングの設定を変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  get '/XXX', to: 'AAA#bbb'
+　get '/topics/new', to: 'topics#new'
+　get '/topics/edit', to: 'topics#edit'
+
 end
