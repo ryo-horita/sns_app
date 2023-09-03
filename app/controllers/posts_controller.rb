5eedf9391916a200aa1c6cafe25ad35266c20976
ryohorita <ryopiko2021@gmail.com>
2023-09-03 10:45:40 +0000

    9月3日

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
-  def new
-    render :new
-  end
+  before_action :authenticate_user!
   def index
-    render :index
-  end  
+    render :index # renders app/views/posts/index.html.erb
+  end
+
+  def new
+    render :new # renders app/views/posts/new.html.erb
+  end
+
+  # このActionを追加
+  def create
+    redirect_to new_post_path # redirects to GET "/posts/new"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   # PostsController
   get '/', to: 'posts#index', as: 'index_post'
   get 'posts/new', to: 'posts#new', as: 'new_post'
-  get '/topics/new', to: 'topics#new', as: 'new_topics'
-  get '/topics/edit', to: 'topics#edit', as: 'edit_topics'
+  # この行を追加
+  post 'posts/new', to: 'posts#create', as: 'create_post'
+
+  # TopicsController
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
+  get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topic'
+
 end
