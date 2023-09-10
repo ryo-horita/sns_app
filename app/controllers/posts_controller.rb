ae29f0d92ace757c43c5f54e0894f7f656d32fa2
ryohorita <ryopiko2021@gmail.com>
2023-09-10 04:56:27 +0000

    9月11日

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,17 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
 
+  # このActionを編集してください
   def index
-    render 'posts/index'
-  end
-
-  # ここから
-  def new
-    @post = Post.new
-    render :new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    if params[:post][:image]
-      @post.image.attach(params[:post][:image])
-    end
-
-    if @post.save
-      redirect_to index_post_path, notice: '登録しました'
+    @title = params[:title]
+    if @title.present?
+      @posts = Post.where('title LIKE ?', "%#{@title}%")
     else
-      render :new, status: :unprocessable_entity
+      @posts = Post.all
     end
-  end
-
-  private
-  def post_params
-    params.require(:post).permit(:title, :body, :image)
+    render :index
   end
   # ここまで
+
+  # 以下既存コード
 end
