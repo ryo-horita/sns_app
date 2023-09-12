ec66599d1b7b6773f35811b8464e73051d7274b6
ryohorita <ryopiko2021@gmail.com>
2023-09-12 11:03:23 +0000

    9月12日

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  # 既存コード
 
-  # このActionを編集してください
+  # ここから追加
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to index_post_path, notice: '削除しました'
+  end
+  # ここまで
+
+  # 既存コード
+
+
   def index
     @title = params[:title]
     if @title.present?
@@ -11,7 +22,47 @@ class PostsController < ApplicationController
     end
     render :index
   end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+
+    if @post.save
+      redirect_to index_post_path, notice: '投稿しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # ここから追加
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+    if @post.update(post_params)
+      redirect_to index_post_path, notice: '更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   # ここまで
 
-  # 以下既存コード
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'posts#index', as: 'index_post'
 
   # PostsController
-  get '/', to: 'posts#index', as: 'index_post'
   get 'posts/new', to: 'posts#new', as: 'new_post'
-  # この行を追加
   post 'posts/new', to: 'posts#create', as: 'create_post'
+  get 'posts/edit/:id', to: 'posts#edit', as: 'edit_post'
+  post 'posts/edit/:id', to: 'posts#update', as: 'update_post'
+  # この行を追加
+  delete 'posts/destroy/:id', to: 'posts#destroy', as: 'destroy_post'
 
   # TopicsController
   get 'topics/new', to: 'topics#new', as: 'new_topic'
+  post '/topics/new', to: 'topics#create', as: 'create_topic'
   get 'topics/edit/:id', to: 'topics#edit', as: 'edit_topic'
+  post '/topics/edit/:id', to: 'topics#update', as: 'update_topic'
 
 end
